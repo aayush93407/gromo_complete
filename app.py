01ed708c111a0bb76b543ada20ce0d3499e44051
aayush93407 <92848064+aayush93407@users.noreply.github.com>
2025-05-20 14:26:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def predict_result():
     
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))  # Render sets PORT env var
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
 
