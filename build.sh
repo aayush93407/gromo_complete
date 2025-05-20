f6ccd03f4fbb12618c9c56d50dbba0ec32f08dbb
aayush93407 <92848064+aayush93407@users.noreply.github.com>
2025-05-20 16:27:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-set -o errexit
 
-# Install Python dependencies
+# Render custom build script
+echo "Installing system packages..."
+apt-get update && apt-get install -y ffmpeg
+
+echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
-# Download precompiled ffmpeg binary
-mkdir -p ffmpeg
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz -o ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz --strip-components=1 -C ffmpeg
-chmod +x ffmpeg/ffmpeg
-
-# Add ffmpeg to PATH so it works in subprocess or os.system
-export PATH="$PATH:$PWD/ffmpeg"
+echo "Downloading SpaCy model..."
+python -m spacy download en_core_web_sm
